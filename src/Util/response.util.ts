736acc1b688aa233c6e4b518668eefeb966b7abc
VZade1999 <vzade1999@gmail.com>
2026-08-08 02:47:43 +0530

    fix logout time

--- src/Util/response.util.ts
+++ src/Util/response.util.ts
@@ -43,10 +43,12 @@ const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || 'access-secret';
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'refresh-secret';
 
 export const generateAccessToken = (payload: Record<string, any>) =>
-  jwt.sign(payload, ACCESS_SECRET, { expiresIn: '15m' });
+  // jwt.sign(payload, ACCESS_SECRET, { expiresIn: '1m' });
+ jwt.sign(payload, ACCESS_SECRET);
 
 export const generateRefreshToken = (payload: Record<string, any>) =>
-  jwt.sign(payload, REFRESH_SECRET, { expiresIn: '7d' });
+  // jwt.sign(payload, REFRESH_SECRET, { expiresIn: '7d' });
+  jwt.sign(payload, REFRESH_SECRET);
 
 export const verifyAccessToken = (token: string) => jwt.verify(token, ACCESS_SECRET);
 
